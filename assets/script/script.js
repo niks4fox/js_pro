26d84f8ce1781863eae6d2319f55997ea4310302
niks4fox <nikhil.4fox@gmail.com>
2021-10-19 16:03:54 +0530

    commit

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,39 +1,59 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
-
+let logEntries = [];
 function getUserNumInput() {
     return parseInt(usrInput.value)
 }
-
-function createrAndWriteOutput(operator, resultBeforCalc, calcNum){
+function createrAndWriteOutput(operator, resultBeforCalc, calcNum) {
     const calcDisc = `${resultBeforCalc} ${operator} ${calcNum}`;
     outputResult(currentResult, calcDisc);
 }
 
+function writeToLog(
+    operationId,
+    prevResult,
+    operationalNum,
+    newResult
+) {
+    const logEntry = {
+        operation: operationId,
+        prevResult: prevResult,
+        number: operationalNum,
+        result: newResult
+    };
+
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
 function add() {
     const enteredNum = getUserNumInput();
-    const initialResult= currentResult
+    const initialResult = currentResult;
     currentResult = currentResult + enteredNum;
     createrAndWriteOutput('+', initialResult, enteredNum);
+    writeToLog('ADD', initialResult, enteredNum, currentResult);
+
 }
 function sub() {
     const enteredNum = getUserNumInput();
-    const initialResult= currentResult;
+    const initialResult = currentResult;
     currentResult = currentResult - enteredNum;
     createrAndWriteOutput('-', initialResult, enteredNum);
-
+    writeToLog('SUB', initialResult, enteredNum, currentResult);
 }
 function multi() {
     const enteredNum = getUserNumInput();
-    const initialResult= currentResult;
+    const initialResult = currentResult;
     currentResult = currentResult * enteredNum;
     createrAndWriteOutput('*', initialResult, enteredNum);
+    writeToLog('MULTI', initialResult, enteredNum, currentResult);
 }
 function divi() {
     const enteredNum = getUserNumInput();
-    const initialResult= currentResult;
+    const initialResult = currentResult;
     currentResult = currentResult / enteredNum;
     createrAndWriteOutput('/', initialResult, enteredNum);
+    writeToLog('DIV', initialResult, enteredNum, currentResult);
 }
 addBtn.addEventListener('click', add);
 subBtn.addEventListener('click', sub);
